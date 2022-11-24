45ff88052aa696a98f7654ad309f6d88894b9712
Axel Bagenal <axel.bagenal@gmail.com>
2022-11-24 17:07:37 +0100

    create bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -14,7 +14,7 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
-      redirect_to list_path(@list)
+      redirect_to @list
     else
       render :new
     end
